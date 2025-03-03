fa5e70d3ed7d5dea723d0f28f809c11db3102a56
Banjanin Dusan <dusan.banjanin@giga.gigatronshop.com>
2025-03-03 10:30:24 +0100

    Resen particles.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,37 @@
-const particlesJS = require("particles.js");
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM je učitan!");
 
-console.log("Particles.js je uvezen!");
+  // Inicijalizacija particles.js
+  particlesJS.load("particles-js", "/js/particles-config.json", function () {
+    console.log("Particles.js je učitan!");
+  });
 
-// Inicijalizacija particles.js
-particlesJS.load("particles-js", "/js/particles-config.json", function () {
-  console.log("Particles.js je učitan!");
-});
+  // Ostali JavaScript kod (npr. za slanje forme)
+  document.getElementById("scrapeForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    console.log("Form submitted!");
 
-// Ostali JavaScript kod (npr. za slanje forme)
-document.getElementById("scrapeForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  console.log("Form submitted!");
+    const url = document.getElementById("urlInput").value;
 
-  const url = document.getElementById("urlInput").value;
+    try {
+      console.log("Sending fetch request...");
+      const response = await fetch("/scrape", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `url=${encodeURIComponent(url)}`,
+      });
 
-  try {
-    console.log("Sending fetch request...");
-    const response = await fetch("/scrape", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `url=${encodeURIComponent(url)}`,
-    });
-
-    console.log("Response received:", response);
-    if (response.ok) {
-      // Redirekcija na stranicu sa rezultatima
-      window.location.href = "/results";
-    } else {
-      console.log("Došlo je do greške pri skrejpovanju.");
+      console.log("Response received:", response);
+      if (response.ok) {
+        // Redirekcija na stranicu sa rezultatima
+        window.location.href = "/results";
+      } else {
+        console.log("Došlo je do greške pri skrejpovanju.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
     }
-  } catch (error) {
-    console.error("Error:", error);
-  }
+  });
 });
